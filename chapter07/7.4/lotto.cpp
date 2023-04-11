4b843386b9e384c296a61bd1e04870a08c34a924
Sticky-Finger <wpwwanpengwang@163.com>
2023-04-11 13:47:25 +0800

    Feat: code for 7.4

---
+++ chapter07/7.4/lotto.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+long double probability(unsigned numbers, unsigned picks);
+
+int main() {
+  using namespace std;
+  double total, choices;
+  cout << "Enter the total number of choices on the game card and\n"
+    "the number of picks allowed:\n";
+  while ((cin >> total >> choices) && choices <= total) {
+    cout << "You have one chance in ";
+    cout << probability(total, choices);
+    cout << " of winning.\n";
+    cout << "Next two numbers (q to quit): ";
+  }
+  cout << "bye\n";
+  return 0;
+}
+
+// the following function calculates the probability of picking picks
+// numbers correctly from numbers choices
+long double probability(unsigned numbers, unsigned picks) {
+  long double result = 0.0;
+  long double n;
+  unsigned p;
+
+  for (n = numbers, p = picks; p > 0; n--, p--)
+    result = result * n / p;
+
+  return 0;
+}
